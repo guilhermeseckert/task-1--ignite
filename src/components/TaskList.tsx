7cd062bd635e85ce783b20c5f085cc9cde520322
Guilherme Eckert <guilherme-sonnemann@hotmail.com>
2021-04-09 15:09:58 -0700

    challemge finzalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-
-import '../styles/tasklist.scss'
+import uuid from 'uuid';
+ import '../styles/tasklist.scss'
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 
@@ -15,15 +15,35 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+  
+    if(!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks(oldValue => [...oldValue, newTask]);
+    setNewTaskTitle("");
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const isComplete = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    } : task )
+    setTasks(isComplete);
   }
+
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(task => task.id !== id);
+    setTasks(removeTask);
+
   }
 
   return (
